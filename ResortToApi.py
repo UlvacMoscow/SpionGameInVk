fb2816a6a037c233908c52a66ab6e9527952c3c9
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2018-04-24 21:24:10 +0300

    update ResortToApi v1.00

--- ResortToApi.py
+++ ResortToApi.py
@@ -1,69 +1,94 @@
-from urllib.parse import urlencode
 from pprint import pprint
 import requests
-
-USER_NAME = 'tim_leary'
-USER_ID = '5030613'
-USER_TOKEN = '7b23e40ad10e08d3b7a8ec0956f2c57910c455e886b480b7d9fb59859870658c4a0b8fdc4dd494db19099'
-
-AUTH_URL ='https://oauth.vk.com/authorize'
-APP_ID = '6417385'
-API_VK = 'https://api.vk.com/method/friends.get'
-
-def get_token():
-    auth_data ={
-        'client_id' : APP_ID,
-        'display' : 'mobile',
-        'scope' : 'friends, groups',
-        'response_type' : 'token',
-        'v' : '5.73',
-    }
-
-    print('?'.join((AUTH_URL, urlencode(auth_data))))
+import json
+import time
+from PackageData.Config import USER_ID, USER_TOKEN, FREEZING
 
 
-# MY_TOKEN = '081932010981455846063c4fa7738184c4355b36ca81be6c778e5633c2e118c9a373ef648d07f01506d87'
-# MY_ID = '135282071'
-def get_friends():
 
-    params = {
-        'access_token' : '7b23e40ad10e08d3b7a8ec0956f2c57910c455e886b480b7d9fb59859870658c4a0b8fdc4dd494db19099',
-        'client_id' : '135282071',
-        'count' : '1000',
-        'v' : '5.74'
-    }
+class User:
+    def get_friends(self, user_token, user_id):
 
-    response = requests.get('https://api.vk.com/method/friends.get', params)
-    pprint(response.text)
+        params = {
+            'access_token' : user_token,
+            'user_id' : user_id,
+            'count' : '1000',
+            'v' : '5.74'
+        }
+        return requests.get('https://api.vk.com/method/friends.get', params)
 
-def get_groups():
-        # 'access_token' : '081932010981455846063c4fa7738184c4355b36ca81be6c778e5633c2e118c9a373ef648d07f01506d87',
-        # 'client_id' : '135282071',
 
-    params = {
-        'access_token': USER_TOKEN,
-        'user_id': USER_ID,
-        'count' : '1000',
-        'v': '5.73'
-    }
+    def get_groups(self, user_token, user_id):
+        params = {
+            'access_token' : user_token,
+            'user_id' : user_id,
+            'count' : '1000',
+            'v' : '5.73'
+        }
+        return requests.get('https://api.vk.com/method/groups.get', params)
 
-    response = requests.get('https://api.vk.com/method/groups.get', params)
-    pprint(response.text)
 
-get_groups()
-# get_friends()
+    def get_info_group(self, user_token, user_id):
+        params = {
+            'access_token': user_token,
+            'user_id': user_id,
+            'group_ids' : '51766355',
+            'v': '5.73'
+        }
+        return requests.get('https://api.vk.com/method/groups.getById', params)
 
-group_id = [51766355,164800385,87389803,35486626,8564,4100014,151498735,41771674,124656670,30314549]
-group_friends = [15628,23059,45857,65668,88364,157312,187268,212696,239216,252651,256433,301147,321229,325712,326136]
 
-# params = {
-#     'access_token': USER_TOKEN,
-#     'client_id': USER_ID,
-#     'group_id' : '51766355',
-#     'extended': '0',
-#     'count': '1000',
-#     'v': '5.73'
-# }
-#
-# response = requests.get('https://api.vk.com/method/groups.getById', params)
-# pprint(response.text)
+    def get_count_members(self, user_token, user_id):
+        params = {
+            'access_token':  user_token,
+            'user_id': user_id,
+            'group_id': '51766355',
+            'v': '5.73'
+        }
+        return requests.get('https://api.vk.com/method/groups.getMembers', params)
+
+
+user = User()
+start_time = time.time()
+result_groups = json.loads(user.get_groups(USER_TOKEN, USER_ID).text)['response']['items']
+result_groups = set(result_groups)
+time.sleep(FREEZING)
+user_friends = json.loads(user.get_friends(USER_TOKEN, USER_ID).text)
+full_time = len(user_friends['response']['items']) * FREEZING * 1.5
+print('у данного юзера {} групп и {} друзей'.format(len(result_groups),len(user_friends['response']['items'])))
+print("Примерное время выполнения программы {} секунд".format(round(full_time, 0)))
+time.sleep(FREEZING)
+
+
+for friend in user_friends['response']['items']:
+    try:
+        friend_groups = json.loads(user.get_groups(USER_TOKEN, friend).text)
+        time.sleep(FREEZING)
+        # print(friend_groups)
+        friend_groups = set(friend_groups['response']['items'])
+        result_groups = result_groups - friend_groups
+        print('до конца осталось {} секунд'.format(round(full_time - (time.time() - start_time)), 0))
+    except KeyError:
+        print("Нет доступа к группе пользователя")
+
+
+print("Шпионских групп ", len(result_groups))
+result_groups_json = {'gid' : list(result_groups)}
+result_groups_json = json.dumps(result_groups_json)
+pprint(result_groups_json)
+print('Время выполнения программы {} секунд'.format(round(time.time() - start_time), 0))
+
+# print(user.get_info_group(USER_TOKEN, USER_ID).text)
+# print(user.get_count_members(USER_TOKEN, USER_ID).text)
+
+
+
+
+
+
+
+
+
+
+
+
