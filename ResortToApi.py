567b61e857cdefe0eb2f83fa7d1b0f1c46c7d7d3
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2018-04-27 19:47:29 +0300

    update ResortToApi v1.03 add check_groups_friends, fill_the_json, save json

--- ResortToApi.py
+++ ResortToApi.py
@@ -1,54 +1,74 @@
-from pprint import pprint
-
-import chardet
-import requests
 import json
 import time
+
+import requests
 from PackageData.Config import USER_ID, USER_TOKEN, FREEZING
 
 
-
 class User:
-    def get_friends(self, user_id):
 
+    def get_friends(self, user_id):
         params = {
-            'access_token' : USER_TOKEN,
-            'user_id' : user_id,
-            'count' : '1000',
-            'v' : '5.74'
+            'access_token': USER_TOKEN,
+            'user_id': user_id,
+            'count': '1000',
+            'v': '5.74'
         }
         return requests.get('https://api.vk.com/method/friends.get', params)
 
-
     def get_groups(self, user_id):
         params = {
-            'access_token' : USER_TOKEN,
-            'user_id' : user_id,
-            'count' : '1000',
-            'v' : '5.73'
+            'access_token': USER_TOKEN,
+            'user_id': user_id,
+            'count': '1000',
+            'v': '5.73'
         }
         return requests.get('https://api.vk.com/method/groups.get', params)
-
 
     def get_info_group(self, user_id, group_id):
         params = {
             'access_token': USER_TOKEN,
             'user_id': user_id,
-            'group_ids' : group_id,
+            'group_ids': group_id,
             'v': '5.73'
         }
         return requests.get('https://api.vk.com/method/groups.getById', params)
 
-
     def get_count_members(self, user_id, group_id):
 
         params = {
-            'access_token':  USER_TOKEN,
+            'access_token': USER_TOKEN,
             'user_id': user_id,
             'group_id': group_id,
             'v': '5.73'
         }
         return requests.get('https://api.vk.com/method/groups.getMembers', params)
+
+    def check_groups_friends(self, group_friends, groups):
+        for friend in group_friends:
+            try:
+                friend_groups = json.loads(user.get_groups(friend).text)
+                time.sleep(FREEZING)
+                friend_groups = set(friend_groups['response']['items'])
+                groups -= friend_groups
+                print('до конца осталось {} секунд'.format(round(full_time - (time.time() - start_time)), 0))
+            except KeyError:
+                print("Нет доступа к группе пользователя")
+        return groups
+
+    def fill_the_json(self, groups):
+        for group in groups:
+            try:
+                name = json.loads(user.get_info_group(USER_ID, group).text)
+                time.sleep(FREEZING)
+                count = json.loads(user.get_count_members(USER_ID, group).text)
+                finish_group_info.append({'name': name['response'][0]['name'],
+                                          'gid': group,
+                                          'members_count': count['response']['count']})
+                time.sleep(FREEZING)
+                print('до конца осталось {} секунд'.format(round(full_time - (time.time() - start_time)), 0))
+            except KeyError:
+                print('нет доступа к информации группы')
 
 
 user = User()
@@ -64,66 +84,13 @@ print('у данного юзера {} групп и {} друзей'.format(len
                                                     len(user_friends['response']['items'])))
 print("Примерное время выполнения программы {} секунд".format(round(full_time, 0)))
 time.sleep(FREEZING)
-
-
-for friend in user_friends['response']['items']:
-    try:
-        friend_groups = json.loads(user.get_groups(friend).text)
-        time.sleep(FREEZING)
-        # print(friend_groups)
-        friend_groups = set(friend_groups['response']['items'])
-        result_groups = result_groups - friend_groups
-        print('до конца осталось {} секунд'.format(round(full_time - (time.time() - start_time)), 0))
-    except KeyError:
-        print("Нет доступа к группе пользователя")
-
-
+result_groups = user.check_groups_friends(user_friends['response']['items'], result_groups)
 print("Шпионских групп ", len(result_groups))
 
-# result_groups_json = {'gid' : list(result_groups)}
-# result_groups_json = json.dumps(result_groups_json)
-# pprint(result_groups_json)
-
-# name = json.loads(user.get_info_group(USER_ID).text)
-#
-# print('here', name)
-#
-# print(name['response'][0]['name'])
-# time.sleep(FREEZING)
-# count = json.loads(user.get_count_members(USER_ID).text)
-# print(count['response']['count'])
-#
-# print(count)
-
-
-for group in result_groups:
-    try:
-        name = json.loads(user.get_info_group(USER_ID, group).text)
-        time.sleep(FREEZING)
-        count = json.loads(user.get_count_members(USER_ID, group).text)
-        finish_group_info.append({'name' : name['response'][0]['name'],
-                                  'gid' : group,
-                                  'members_count' : count['response']['count']})
-        time.sleep(FREEZING)
-        print('до конца осталось {} секунд'.format(round(full_time - (time.time() - start_time)), 0))
-    except KeyError:
-        print('нет доступа к информации группы')
-
+user.fill_the_json(result_groups)
 
 print(finish_group_info)
-finish_group_info = json.dumps(finish_group_info)
-pprint(finish_group_info)
-
+with open('spion_game.json', "w", encoding="utf-8") as file:
+    json.dump(finish_group_info, file, indent=3, ensure_ascii=False)
 
 print('Время выполнения программы {} секунд'.format(round(time.time() - start_time), 0))
-
-
-
-
-
-
-
-
-
-
-
